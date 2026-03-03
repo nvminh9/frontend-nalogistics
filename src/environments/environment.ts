cc01ca69d5cb384203ce34ca85c347c9f09fab9d
ngoducthinh <ngoducthinhit07!@gmail.com>
2026-03-03 22:34:45 +0700

    Update : change port 9999 to 80 for Angular container

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://103.163.119.173:9999/api/',
-    socketUrl: 'ws://103.163.119.173:9999/api/',
+    apiUrl: 'http://103.163.119.173/api/',
+    socketUrl: 'ws://103.163.119.173/api/',
     OrderStatusPattern: [
         { key: 0, value: "Đang chờ" },
         { key: 1, value: "Đang xử lý" },
